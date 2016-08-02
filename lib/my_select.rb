d53c19db085f46c53436336d14a06cf61f22507a
Wesley Friedman <friedman.wesley@gmail.com>
2016-08-02 23:59:09 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  stored_nums = []
+  if block_given?
+    i = 0
+    while i < collection.length
+      if yield(collection[i])
+        stored_nums << collection[i]
+      end
+      i = i + 1
+    end
+    return stored_nums
+  end
 end
